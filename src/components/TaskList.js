28cf07d7ab64eed98d6acd2863d518303ce95bfc
Kepler <hanif.hg@gmail.com>
2018-05-26 16:07:52 +0700

    Create label with jsx

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,16 +4,29 @@ import CloseButton from './utilities/CloseButton'
 
 export default class TaskList extends React.Component {
 
-  // static propTypes = {
-  //     todos: React.PropTypes.array.isRequired
-  // }
-
+  TaskLabel (todo) {
+    if (todo.completed) {
+      return (
+        <span>
+          <del>
+            { todo.task}
+          </del>
+        </span>
+      )
+    } else {
+      return (
+        <span>{ todo.task}</span>
+      )
+    }
+  }
+  
   render() {
     return (
       <div>
         { this.props.todos.map((todo, index) => ( 
           <div key={ index } className="clearfix">
             <Checkbox className="float-left" checked={ todo.completed } label={ todo.task } onClick={ this.props.completeTask.bind(this, index) } />
+            { this.TaskLabel(todo) }
             <CloseButton className="float-right" />
           </div>
         )) }
