7933fce986d30d1f52fd3e9884ea4f378b8f1300
Kepler <hanif.hg@gmail.com>
2018-05-26 18:16:51 +0700

    Bind even delete

--- src/components/utilities/CloseButton.js
+++ src/components/utilities/CloseButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class CloseButton extends React.Component {
   render() {
     return (
-      <button type="button" className="close" aria-label="Close">
+      <button type="button" className="close" aria-label="Close" onClick={ this.props.onClick.bind(this) }>
         <span aria-hidden="true">&times;</span>
       </button>
     )
